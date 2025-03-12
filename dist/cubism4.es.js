cc263aa19e15fd5154cb57bc6c870cbb471370a1
Stephen Lizcano <stephen@lizcano.dev>
2025-03-12 14:25:05 -0700

    Fix: Apply latest code updates and improvements across Cubism versions and core functionalities.

--- dist/cubism4.es.js
+++ dist/cubism4.es.js
@@ -1,10 +1,7 @@
 var __defProp = Object.defineProperty;
 var __pow = Math.pow;
 var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
-var __publicField = (obj, key, value) => {
-  __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
-  return value;
-};
+var __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
 var __async = (__this, __arguments, generator) => {
   return new Promise((resolve, reject) => {
     var fulfilled = (value) => {
@@ -1155,8 +1152,7 @@ class CubismRenderer {
    * モデルを描画する
    */
   drawModel() {
-    if (this.getModel() == null)
-      return;
+    if (this.getModel() == null) return;
     this.saveProfile();
     this.doDrawModel();
     this.restoreProfile();
@@ -1507,8 +1503,7 @@ class CubismDebug {
       return;
     }
     const logPrint = CubismFramework.coreLogFunction;
-    if (!logPrint)
-      return;
+    if (!logPrint) return;
     const buffer = format.replace(/{(\d+)}/g, (m, k) => {
       return args[k];
     });
@@ -1524,10 +1519,8 @@ class CubismDebug {
    */
   static dumpBytes(logLevel, data, length) {
     for (let i = 0; i < length; i++) {
-      if (i % 16 == 0 && i > 0)
-        this.print(logLevel, "\n");
-      else if (i % 8 == 0 && i > 0)
-        this.print(logLevel, "  ");
+      if (i % 16 == 0 && i > 0) this.print(logLevel, "\n");
+      else if (i % 8 == 0 && i > 0) this.print(logLevel, "  ");
       this.print(logLevel, "{0} ", [data[i] & 255]);
     }
     this.print(logLevel, "\n");
@@ -6222,8 +6215,7 @@ class CubismClippingManager_WebGL {
         if (layoutCount < layoutCountMaxValue && channelNo == checkChannelNo) {
           layoutCount += renderTextureNo < countPerSheetMod ? 1 : 0;
         }
-        if (layoutCount == 0)
-          ;
+        if (layoutCount == 0) ;
         else if (layoutCount == 1) {
           const clipContext = this._clippingContextListForMask[curClipIndex++];
           clipContext._layoutChannelNo = channelNo;
@@ -6409,16 +6401,12 @@ class CubismClippingContext {
 }
 class CubismRendererProfile_WebGL {
   setGlEnable(index, enabled) {
-    if (enabled)
-      this.gl.enable(index);
-    else
-      this.gl.disable(index);
+    if (enabled) this.gl.enable(index);
+    else this.gl.disable(index);
   }
   setGlEnableVertexAttribArray(index, enabled) {
-    if (enabled)
-      this.gl.enableVertexAttribArray(index);
-    else
-      this.gl.disableVertexAttribArray(index);
+    if (enabled) this.gl.enableVertexAttribArray(index);
+    else this.gl.disableVertexAttribArray(index);
   }
   save() {
     if (this.gl == null) {
@@ -7930,6 +7918,7 @@ class ExpressionManager extends utils.EventEmitter {
    */
   resetExpression() {
     this._setExpression(this.defaultExpression);
+    this.currentExpression = this.defaultExpression;
   }
   /**
    * Restores model's expression to {@link currentExpression}.
@@ -8025,8 +8014,7 @@ class FocusController {
   update(dt) {
     const dx = this.targetX - this.x;
     const dy = this.targetY - this.y;
-    if (Math.abs(dx) < EPSILON && Math.abs(dy) < EPSILON)
-      return;
+    if (Math.abs(dx) < EPSILON && Math.abs(dy) < EPSILON) return;
     const d = Math.sqrt(__pow(dx, 2) + __pow(dy, 2));
     const maxSpeed = MAX_SPEED / (1e3 / dt);
     let ax = maxSpeed * (dx / d) - this.vx;
@@ -9260,14 +9248,11 @@ function runMiddlewares(middleware, context) {
   let index = -1;
   return dispatch(0);
   function dispatch(i, err) {
-    if (err)
-      return Promise.reject(err);
-    if (i <= index)
-      return Promise.reject(new Error("next() called multiple times"));
+    if (err) return Promise.reject(err);
+    if (i <= index) return Promise.reject(new Error("next() called multiple times"));
     index = i;
     const fn = middleware[i];
-    if (!fn)
-      return Promise.resolve();
+    if (!fn) return Promise.resolve();
     try {
       return Promise.resolve(fn(context, dispatch.bind(null, i + 1)));
     } catch (err2) {
@@ -10582,8 +10567,7 @@ class Cubism4InternalModel extends InternalModel {
   getDrawableVertices(drawIndex) {
     if (typeof drawIndex === "string") {
       drawIndex = this.coreModel.getDrawableIndex(drawIndex);
-      if (drawIndex === -1)
-        throw new TypeError("Unable to find drawable ID: " + drawIndex);
+      if (drawIndex === -1) throw new TypeError("Unable to find drawable ID: " + drawIndex);
     }
     const arr = this.coreModel.getDrawableVertices(drawIndex).slice();
     for (let i = 0; i < arr.length; i += 2) {
