e71725005ca185b601f725ffde05e13460e9cb46
张卓诚 <shiftonetothree@126.com>
2025-03-27 00:51:27 +0800

    feat(stability.test.ts): fix ci

--- test/features/stability.test.ts
+++ test/features/stability.test.ts
@@ -2,15 +2,17 @@ import { testEachModel } from "../env";
 import { createModel } from "../utils";
 import { makeTestSound } from "../make-test-sound";
 
+const times = 500;
+
 testEachModel(
-    "speak 1000 times",
+    `speak ${times} times`,
     async ({ app, model: { modelJsonWithUrl, hitTests } }) => {
         const model = await createModel(modelJsonWithUrl);
         model.update(100);
         app.stage.addChild(model);
         app.renderer.resize(model.width, model.height);
         app.render();
-        for (let i = 0; i < 1000; i++) {
+        for (let i = 0; i < times; i++) {
             await new Promise<void>((resolve, reject) => {
                 const duration = Math.random() * 0.01;
                 model.speak(makeTestSound(undefined, 0.1, duration), {
